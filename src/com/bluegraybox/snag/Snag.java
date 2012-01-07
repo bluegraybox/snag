5548a87a2bb5ae2e0ebfd748fa531b27c9bbb1e7
Colin MacDonald <colin@bluegraybox.com>
2012-01-07 13:21:06 -0500

    Initial check-in of autogenerated base project.

---
+++ src/com/bluegraybox/snag/Snag.java
@@ -0,0 +1,13 @@
+package com.bluegraybox.snag;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Snag extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
